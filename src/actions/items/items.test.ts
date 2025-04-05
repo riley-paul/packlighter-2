78a508940e0dc00ebfeea4ee12c470304ca56f0c
Riley Paul <rileypaul96@gmail.com>
2025-04-04 23:09:46 -0600

    build out list in fixtures

--- src/actions/items/items.test.ts
+++ src/actions/items/items.test.ts
@@ -1,12 +1,12 @@
 import { expect, test, describe } from "vitest";
 import mockApiContext from "@/tests/mock-api-context";
 import itemHandlers from "./items.handlers";
-import { USER_ID } from "@/tests/fixtures";
+import { ITEM_IDS, USER_ID } from "@/tests/fixtures";
 
 describe("get all items", () => {
   test("returns all of the users items", async () => {
-    const todos = await itemHandlers.getAll(null, mockApiContext(USER_ID));
-    expect(Array.isArray(todos)).toBe(true);
-    expect(todos.length).toBe(0);
+    const items = await itemHandlers.getAll(null, mockApiContext(USER_ID));
+    expect(Array.isArray(items)).toBe(true);
+    expect(items.length).toBe(ITEM_IDS.length);
   });
 });
--- src/tests/fixtures.ts
+++ src/tests/fixtures.ts
@@ -1,8 +1,21 @@
 import { createDb } from "@/db";
-import { User } from "@/db/schema";
+import { Category, CategoryItem, Item, List, User } from "@/db/schema";
 import env from "@/envs-runtime";
+import type {
+  CategoryInsert,
+  CategoryItemInsert,
+  ItemInsert,
+} from "@/lib/types";
+
+const getRandomArrayItem = <T>(arr: T[]): T => {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+};
 
 export const USER_ID = crypto.randomUUID();
+export const LIST_ID = crypto.randomUUID();
+export const ITEM_IDS = new Array(20).fill(0).map(() => crypto.randomUUID());
+export const CATEGORY_IDS = new Array(5).fill(0).map(() => crypto.randomUUID());
 
 export const seedTestData = async () => {
   const db = createDb(env);
@@ -12,4 +25,43 @@ export const seedTestData = async () => {
     name: "Test User",
     email: "test-user@example.com",
   });
+
+  await db.insert(Item).values(
+    ITEM_IDS.map(
+      (itemId): ItemInsert => ({
+        id: itemId,
+        name: "Test Item",
+        description: "This is a test item",
+        userId: USER_ID,
+      }),
+    ),
+  );
+
+  await db.insert(List).values({
+    id: LIST_ID,
+    userId: USER_ID,
+    name: "Test List",
+    description: "This is a test list",
+  });
+
+  await db.insert(Category).values(
+    CATEGORY_IDS.map(
+      (categoryId): CategoryInsert => ({
+        id: categoryId,
+        name: "Test Category",
+        listId: LIST_ID,
+        userId: USER_ID,
+      }),
+    ),
+  );
+
+  await db.insert(CategoryItem).values(
+    ITEM_IDS.map(
+      (item_id): CategoryItemInsert => ({
+        itemId: item_id,
+        categoryId: getRandomArrayItem(CATEGORY_IDS),
+        userId: USER_ID,
+      }),
+    ),
+  );
 };
