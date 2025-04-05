d102969d4cd1c484e7bc70294ad6e649436fc559
Riley Paul <rileypaul96@gmail.com>
2025-04-04 21:54:29 -0600

    update init

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -1,63 +1,55 @@
 import {
-  zCategoryItemSelect,
-  zCategorySelect,
-  zItemSelect,
   type ExpandedCategory,
   type ExpandedCategoryItem,
   type ItemSelect,
 } from "@/lib/types";
 import { v4 as uuid } from "uuid";
-import { z } from "zod";
 
 const MOCK_USER_ID = "mock-user-id";
 const currentTime = () => new Date().toISOString();
 
-const zItemInit = zItemSelect.extend({
-  id: zItemSelect.shape.id.default(uuid),
-  userId: zItemSelect.shape.userId.default(MOCK_USER_ID),
-  name: zItemSelect.shape.name.default(""),
-  description: zItemSelect.shape.description.default(""),
-  weight: zItemSelect.shape.weight.default(0),
-  weightUnit: zItemSelect.shape.weightUnit.default("g"),
-  image: zItemSelect.shape.image.default(""),
-  createdAt: zItemSelect.shape.createdAt.default(currentTime),
-  updatedAt: zItemSelect.shape.updatedAt.default(currentTime),
-});
-
-export const initItem = (data: Partial<ItemSelect> = {}): ItemSelect =>
-  zItemInit.parse(data);
-
-const zCategoryItemInit = zCategoryItemSelect.extend({
-  id: zCategoryItemSelect.shape.id.default(uuid),
-  userId: zCategoryItemSelect.shape.userId.default(MOCK_USER_ID),
-  categoryId: zCategoryItemSelect.shape.categoryId.default(uuid),
-  itemData: zItemInit,
-  quantity: zCategoryItemSelect.shape.quantity.default(1),
-  sortOrder: zCategoryItemSelect.shape.sortOrder.default(1),
-  itemId: zCategoryItemSelect.shape.itemId.default(uuid),
-  packed: zCategoryItemSelect.shape.packed.default(false),
-  wornWeight: zCategoryItemSelect.shape.wornWeight.default(false),
-  consWeight: zCategoryItemSelect.shape.consWeight.default(false),
-  createdAt: zCategoryItemSelect.shape.createdAt.default(currentTime),
-  updatedAt: zCategoryItemSelect.shape.updatedAt.default(currentTime),
+export const initItem = (data: Partial<ItemSelect> = {}): ItemSelect => ({
+  ...data,
+  id: data.id ?? uuid(),
+  userId: data.userId ?? MOCK_USER_ID,
+  name: data.name ?? "",
+  description: data.description ?? "",
+  weight: data.weight ?? 0,
+  weightUnit: data.weightUnit ?? "g",
+  image: data.image ?? null,
+  createdAt: data.createdAt ?? currentTime(),
+  updatedAt: data.updatedAt ?? currentTime(),
 });
 
 export const initCategoryItem = (
   data: Partial<ExpandedCategoryItem> = {},
-): ExpandedCategoryItem => zCategoryItemInit.parse(data);
-
-const zCategoryInit = zCategorySelect.extend({
-  id: zCategorySelect.shape.id.default(uuid),
-  userId: zCategorySelect.shape.userId.default(MOCK_USER_ID),
-  name: zCategorySelect.shape.name.default(""),
-  sortOrder: zCategorySelect.shape.sortOrder.default(1),
-  items: z.array(zCategoryItemInit).default([]),
-  createdAt: zCategorySelect.shape.createdAt.default(currentTime),
-  updatedAt: zCategorySelect.shape.updatedAt.default(currentTime),
-  listId: zCategorySelect.shape.listId.default(uuid),
-  packed: z.boolean().default(false),
+): ExpandedCategoryItem => ({
+  ...data,
+  id: data.id ?? uuid(),
+  userId: data.userId ?? MOCK_USER_ID,
+  categoryId: data.categoryId ?? uuid(),
+  itemData: initItem(data.itemData),
+  quantity: data.quantity ?? 1,
+  sortOrder: data.sortOrder ?? 1,
+  itemId: data.itemId ?? uuid(),
+  packed: data.packed ?? false,
+  wornWeight: data.wornWeight ?? false,
+  consWeight: data.consWeight ?? false,
+  createdAt: data.createdAt ?? currentTime(),
+  updatedAt: data.updatedAt ?? currentTime(),
 });
 
 export const initCategory = (
   data: Partial<ExpandedCategory> = {},
-): ExpandedCategory => zCategoryInit.parse(data);
+): ExpandedCategory => ({
+  ...data,
+  id: data.id ?? uuid(),
+  userId: data.userId ?? MOCK_USER_ID,
+  name: data.name ?? "",
+  sortOrder: data.sortOrder ?? 1,
+  items: data.items?.map((item) => initCategoryItem(item)) ?? [],
+  createdAt: data.createdAt ?? currentTime(),
+  updatedAt: data.updatedAt ?? currentTime(),
+  listId: data.listId ?? uuid(),
+  packed: data.packed ?? false,
+});
