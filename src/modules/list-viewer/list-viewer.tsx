df255fe0b2ee8cdb7116dadc643a67ed0397b7af
Riley Paul <rileypaul96@gmail.com>
2025-05-13 19:59:18 -0600

    add to viewer

--- src/modules/list-viewer/list-viewer.tsx
+++ src/modules/list-viewer/list-viewer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Markdown from "react-markdown";
 import ViewerCategory from "./list-viewer-category";
 import type { ExpandedList } from "@/lib/types";
+import WeightPanel from "../weight-chart/weight-panel";
 
 type Props = {
   list: ExpandedList;
@@ -15,6 +16,7 @@ const ViewerList: React.FC<Props> = (props) => {
       <Markdown className="text-sm prose prose-sm max-w-none px-2 dark:prose-invert">
         {`# ${list.name}\n` + list.description}
       </Markdown>
+      <WeightPanel list={list} />
       {list.categories.map((category) => (
         <ViewerCategory key={category.id} category={category} list={list} />
       ))}
